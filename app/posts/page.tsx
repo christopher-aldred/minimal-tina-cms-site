09edf33fa0abc1cf0e48de064eaf8b63a776f5d4
afluffysquirrel <afluffysquirrel@gmail.com>
2024-10-13 16:44:45 +0100

    chore: clean up

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -8,48 +8,49 @@ interface PaginationProps {
 
 // Page listing all posts
 export default async function Page({ after = new Date() }: PaginationProps) {
-  const itemsPerPage = 2;
+  const ITEMS_PER_PAGE = 12;
   after?.setDate(after.getDate() + 1);
   const { data } = await client.queries.postConnection({
     sort: "added",
-    last: itemsPerPage,
+    last: ITEMS_PER_PAGE,
     filter: { added: { before: after?.toDateString() } },
   });
   let posts = data!.postConnection!.edges!;
 
-  // posts = posts.sort(
-  //   (a, b) =>
-  //     new Date(b?.node?.added!).valueOf() - new Date(a?.node?.added!).valueOf()
-  // );
-
   return (
     <>
       <LinkColourer />
-      {/* <center>
-        <h1>{after?.toDateString()}</h1>
-      </center> */}
+
       {posts.length < 1 ? (
         <center>
           <h1>No posts!</h1>
         </center>
       ) : null}
+
       <div>
         {posts.map((post) => (
           <div className="text-2xl mb-10" key={post!.node!.id}>
             <div className="flex items-baseline justify-between">
+              {/* Title */}
               <Link
                 href={`/posts/${post!.node!._sys.filename}`}
                 className="flex-1"
               >
                 {post!.node!.title}
               </Link>
+
+              {/* Date */}
               <div className="text-sm text-neutral-500 italic ml-2 whitespace-nowrap">
                 {new Date(post!.node!.added).toDateString()}
               </div>
             </div>
+
+            {/* Description */}
             <div className="text-neutral-700 dark:text-neutral-300 text-base my-1">
               {post!.node!.shortDescription}
             </div>
+
+            {/* Tags */}
             <div className="text-xs">
               {post!.node!.tags?.map((tag) => (
                 <span
